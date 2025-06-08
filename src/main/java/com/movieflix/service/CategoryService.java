985228c2496e923f7d1c649e365b1012ad013bc9
HerikMario <herikmariomr@gmail.com>
2025-06-08 00:21:41 -0300

    feat: Adiciona o service com a lógica que alimenta os endpoints

---
+++ src/main/java/com/movieflix/service/CategoryService.java
@@ -0,0 +1,35 @@
+package com.movieflix.service;
+
+import com.movieflix.entity.Category;
+import com.movieflix.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    private CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public Category saveCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Optional<Category> findById(Long id) {
+        return categoryRepository.findById(id);
+    }
+
+    public void deleteByCategoryId(Long id) {
+        categoryRepository.deleteById(id);
+    }
+
+}
