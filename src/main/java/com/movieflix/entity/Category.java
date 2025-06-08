7d98016e6e6c0907fa8781e4db05dcf946cd7247
HerikMario <herikmariomr@gmail.com>
2025-06-08 00:06:58 -0300

    feat: Adiciona a entidade de Category

---
+++ src/main/java/com/movieflix/entity/Category.java
@@ -0,0 +1,22 @@
+package com.movieflix.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_category")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String nome;
+
+}
